51d1bc6f4e1060510bc7fc0fa7a2d67c6d2dfb83
John <72810050+Notplayingallday383@users.noreply.github.com>
2024-05-15 01:47:39 +0000

    init

--- static/assets/-/config.js
+++ static/assets/-/config.js
@@ -1,10 +1,10 @@
 self.__uv$config = {
     prefix: '/a/',
-    bare: '/ov/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/assets/-/handler.js?v=5-5-2024',
-    bundle: '/assets/-/bundle.js?v=5-5-2024',
-    config: '/assets/-/config.js?v=5-5-2024',
-    sw: '/assets/-/sw.js?v=5-5-2024',
+    handler: '/assets/-/handler.js?v=5-14-24',
+    client: '/assets/-/client.js?v=5-14-24',
+    bundle: '/assets/-/bundle.js?v=5-14-24',
+    config: '/assets/-/config.js?v=5-14-24',
+    sw: '/assets/-/sw.js?v=5-14-24',
 };
